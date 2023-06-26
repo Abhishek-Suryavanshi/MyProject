792b49d2889da41551741b17d427756e0adac9c6
Abhishek Suryavanshi <abhisheksuryavanshi2308@gmail.com>
2023-06-26 22:56:13 +0530

    Version-3

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,9 +2,14 @@ const Posts = require('../models/Posts');
 
 module.exports.getProfile = async (req, res, next) => {
     // console.log("Here-2: ", req.user.username);
-    let data = await Posts.find({});
-    res.render('profile', {
-        data,
-        name: req.user.username
-    });
+    if (req.user) {
+        let data = await Posts.find({});
+        res.render('profile', {
+            data,
+            name: req.user.username
+        });
+    }
+    else {
+        res.redirect('/');
+    }
 };
